38b6b483e7486a60d148f0e6252a2c30e0946f55
Athiswaran-SF3980 <athiswaran.v@syncfusion.com>
2025-01-31 14:38:33 +0530

    v28.2.3

---
+++ server/Stay-Reservation/Components/Pages/Index.razor.cs
@@ -0,0 +1,35 @@
+﻿using StayReservation.Data;
+using Syncfusion.Blazor.Calendars;
+using Syncfusion.Blazor.Buttons;
+using Syncfusion.Blazor.Data;
+using Syncfusion.Blazor.Schedule;
+using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+using Syncfusion.Blazor.Inputs;
+using Syncfusion.Blazor.TreeMap;
+using Syncfusion.Blazor.Navigations;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace StayReservation.Components.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public AppointmentService Service { get; set; }
+        internal void StateChanged()
+        {
+            StateHasChanged();
+        }
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Service.IndexPageRef = this;
+            ReservationServices floorService = new ReservationServices();
+            Service.appointmentData = floorService.GenerateStaticEvents(DateTime.Now, 5, 20, 30);
+        }
+    }
+
+}
---
+++ server/Stay-Reservation/Program.cs
@@ -0,0 +1,30 @@
+using StayReservation.Components;
+using StayReservation.Data;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<AppointmentService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
